1ec4e66ae4020fc1f9af786ef617434b8cc2a811
Phong <phong@Phongs-Air.net.fpt>
2019-10-07 12:57:10 +0800

    day1

---
+++ File.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  baitap1
+//
+//  Created by Phong on 10/7/19.
+//  Copyright © 2019 Phong. All rights reserved.
+////
+// bài tâp 1
+//import Foundation
+//func tinhdientich(R: Double) -> Double{
+//    var S: Double
+//    S = (4 * π * R)
+//    print("diện tích hình cầu ")
+//    return pow(S,2)
+//}
+//
+//func tinhthetich(R: Double) -> Double{
+//    var V: Double
+//    V = ((4/3) * π * R)
+//    print("thể tích hình cầu ")
+//    return pow(V,3)
+//}
+
+
+
+//// bài tập 2
+//func tongbinhphuong(a: Double, b: Double) -> Double{
+//    let S: Double
+//    S = (a * a) + (b * b)
+//print( "tổng bình phương a và b")
+//    return S
+//
+//}
+
+
+
+
+//// bài tập 6
+//func phuongtrinh(a: Double,b: Double) -> Double{
+//    var x: Double
+//    (a * x) + b = 0
+//    
+// }
---
+++ main.swift
@@ -0,0 +1,63 @@
+//
+//  main.swift
+//  baitap1
+//
+//  Created by Phong on 10/7/19.
+//  Copyright © 2019 Phong. All rights reserved.
+//
+
+
+//bài tập 1
+//import Foundation
+//let π = 3.14
+//let R = 21
+//let S = tinhdientich
+//print(S)
+//let V = tinhthetich
+//print(V)
+
+
+//// bài tập 2
+//let S = tongbinhphuong(a: 4, b: 5)
+//print(S)
+
+
+////bài tập 3
+//let n: Int = 5
+//if  (n>0) {
+//    print("n là số nguyên dương")
+//}else {
+//print("n không phải là số nguyên dương")
+//}
+//if (n/2) {
+//print("n là số chẵn")}
+//else {
+//print("n là số lẻ")}
+//if (n/5) {
+//    print("n chia hết cho 5")}
+//else {
+//print("n không chia hết cho 5")}
+//if
+
+
+//bài tập 4
+//let a = 24
+//let b = 4
+//if ( a/b) {
+//print("a chia hết cho b")}
+//else {
+//print("a không chia hết cho b")}
+//
+
+
+
+//bài tập 5
+//let a = 7
+//let b = -5
+//if (a){
+//print("max là \(a)")}
+//else if (a>)
+
+
+//bài tập 6
+
